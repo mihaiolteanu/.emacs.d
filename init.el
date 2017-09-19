e9b6b6ff6e82912d304501bf15dd59965450300a
Mihai Olteanu <mihai_olteanu@fastmail.fm>
2017-09-19 11:59:19 +0300

    Eshell modify keybindings and add enhanced ls
    - Move the eshell keybindings in the eshell-mode-map, otherwise they are globel keybindings
    as they were defined until now.
    - Add enhanced eshell ls. After an ls in eshell, you can press <return> on any of the files
    and the file is visited in a buffer.

--- init.el
+++ init.el
@@ -214,15 +214,15 @@
   :ensure t
   :bind ("C-c e" . eshell)
   :config
-  (bind-keys*
-   ("M-r" . helm-eshell-history)
-   ("M-p" . eshell-previous-input)
-   ("M-n" . eshell-next-input)
-   )
   (add-hook 'eshell-mode-hook
 	    (lambda ()
 	      (eshell-cmpl-initialize)
-	      (define-key eshell-mode-map [remap eshell-pcomplete] 'helm-esh-pcomplete))))
+              (smartscan-mode -1)
+              (define-key eshell-mode-map [remap eshell-pcomplete] 'helm-esh-pcomplete)
+              (define-key eshell-mode-map (kbd "M-r") 'helm-eshell-history)
+              (define-key eshell-mode-map (kbd "M-p") 'eshell-previous-input)
+              (define-key eshell-mode-map (kbd "M-n") 'eshell-next-input)
+              )))
 
 (use-package dired-ranger
   :ensure t
@@ -255,6 +255,36 @@
 	 ("C-x C-[" . search-sections)))
 
 ;; §§§ Other stuff
+;; https://www.emacswiki.org/emacs/EshellEnhancedLS
+(eval-after-load "em-ls"
+    '(progn
+       (defun ted-eshell-ls-find-file-at-point (point)
+         "RET on Eshell's `ls' output to open files."
+         (interactive "d")
+         (find-file (buffer-substring-no-properties
+                     (previous-single-property-change point 'help-echo)
+                     (next-single-property-change point 'help-echo))))
+
+       (defun pat-eshell-ls-find-file-at-mouse-click (event)
+         "Middle click on Eshell's `ls' output to open files. From Patrick Anderson via the wiki."
+         (interactive "e")
+         (ted-eshell-ls-find-file-at-point (posn-point (event-end event))))
+
+       (let ((map (make-sparse-keymap)))
+         (define-key map (kbd "RET")      'ted-eshell-ls-find-file-at-point)
+         (define-key map (kbd "<return>") 'ted-eshell-ls-find-file-at-point)
+         (define-key map (kbd "<mouse-2>") 'pat-eshell-ls-find-file-at-mouse-click)
+         (defvar ted-eshell-ls-keymap map))
+
+       (defadvice eshell-ls-decorated-name (after ted-electrify-ls activate)
+         "Eshell's `ls' now lets you click or RET on file names to open them."
+         (add-text-properties 0 (length ad-return-value)
+                              (list 'help-echo "RET, mouse-2: visit this file"
+                                    'mouse-face 'highlight
+                                    'keymap ted-eshell-ls-keymap)
+                              ad-return-value)
+         ad-return-value)))
+
 ;; Disable bars and the blinking cursor
 (mapc
  (lambda (mode)
@@ -285,3 +315,4 @@
 (global-set-key (kbd "<C-tab>") 'other-window)
 (global-set-key (kbd "C-,") 'previous-buffer)
 (global-set-key (kbd "C-.") 'next-buffer)
+
