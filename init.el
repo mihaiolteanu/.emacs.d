df637a84e080a80c80dd09829472840ec725380f
mihaiolteanu <mihai_olteanu@fastmail.fm>
2019-04-02 17:16:47 +0300

    Remove mouse clicks support for ls in eshell (don't need it)

--- init.el
+++ init.el
@@ -426,17 +426,6 @@
                      (previous-single-property-change point 'help-echo)
                      (next-single-property-change point 'help-echo))))
 
-       (defun pat-eshell-ls-find-file-at-mouse-click (event)
-         "Middle click on Eshell's `ls' output to open files. From Patrick Anderson via the wiki."
-         (interactive "e")
-         (ted-eshell-ls-find-file-at-point (posn-point (event-end event))))
-
-       (let ((map (make-sparse-keymap)))
-         (define-key map (kbd "RET")      'ted-eshell-ls-find-file-at-point)
-         (define-key map (kbd "<return>") 'ted-eshell-ls-find-file-at-point)
-         (define-key map (kbd "<mouse-2>") 'pat-eshell-ls-find-file-at-mouse-click)
-         (defvar ted-eshell-ls-keymap map))
-
        (defadvice eshell-ls-decorated-name (after ted-electrify-ls activate)
          "Eshell's `ls' now lets you click or RET on file names to open them."
          (add-text-properties 0 (length ad-return-value)
@@ -444,7 +433,13 @@
                                     'mouse-face 'highlight
                                     'keymap ted-eshell-ls-keymap)
                               ad-return-value)
-         ad-return-value)))
+         ad-return-value)
+       
+       (let ((map (make-sparse-keymap)))
+         (define-key map (kbd "RET")      'ted-eshell-ls-find-file-at-point)
+         (define-key map (kbd "<return>") 'ted-eshell-ls-find-file-at-point)
+         (defvar ted-eshell-ls-keymap map))))
+
 
 ;; Disable bars and the blinking cursor
 (mapc
