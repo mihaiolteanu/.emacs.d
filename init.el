7d7ecc75093f22722e5862ca25cd2cab72a1a3cb
mihaiolteanu <mihai_olteanu@fastmail.fm>
2019-10-25 16:46:07 +0300

    Update sly (remove not needed/used stuff, add hook to sly-mrepl)

--- init.el
+++ init.el
@@ -352,9 +352,9 @@
 (use-package sly
   :config
   (setq inferior-lisp-program "/usr/bin/sbcl")
-
-  ;; Define functionality for interacting with the sly repl using counsel 
+  
   (defun counsel-sly-mrepl-shortcut ()
+    "Interact with the sly repl using counsel."
     (interactive)
     (ivy-read
      "Action: " 
@@ -364,12 +364,6 @@
                                    (cdr (assoc string sly-mrepl-shortcut-alist)))))
                  (call-interactively command)))))
 
-  (defun sly-read-package-name (prompt &optional initial-value allow-blank)
-    (ivy-read
-     "Package: "
-     (sly-eval `(slynk:list-all-package-names t))
-     :action #'identity))
-
   (defun counsel-sly-mrepl-history ()
     (interactive)
     (ivy-read
@@ -378,44 +372,14 @@
      :action (lambda (e)
                (insert e))))
 
-  (defun eval-grab-output (string)
-    (let ((res nil))
-      (sly-eval-async `(slynk:eval-and-grab-output ,string)
-        (lambda (result)
-          (cl-destructuring-bind (output value) result
-            (setf res (car (read-from-string value))))))
-      (while (null res)
-        (sleep-for 0.1))
-      res))
-
-  (defun counsel-sly-eval (string action)
-    (let ((result (eval-grab-output string)))
-      (ivy-read
-       "Symbol: "
-       result
-       :action action)))
-
-  (defun send-input (expr)
-    (insert expr)
-    (comint-send-input))
-
-  (defun counsel-sly-package-internal-symbols ()
-    (interactive)
-    (counsel-sly-eval "(common-lisp-user::package-internal-symbols \*package\*)"
-                      `(1 ("o" ,#'insert "insert")
-                          ("f" ,(lambda (candidate)
-                                  (send-input (format "(find-symbol \"%s\")" candidate)))
-                           "find symbol")
-                          )))
-
-  ;; sly-mrepl-mode-map symbol not available at the time of
-  ;; use-package, so :bind cannot be used here
-  (with-eval-after-load 'sly-mrepl
-    (define-key sly-mrepl-mode-map [remap isearch-backward] 'counsel-sly-mrepl-history)
-    (define-key sly-mrepl-mode-map (kbd ",,") 'counsel-sly-mrepl-shortcut)
-    (define-key sly-mrepl-mode-map (kbd ",p") 'sly-mrepl-set-package)
-    (define-key sly-mrepl-mode-map (kbd ",s") 'counsel-sly-package-internal-symbols)
-    (define-key sly-mrepl-mode-map (kbd ",r") 'sly-restart-inferior-lisp))
+  (add-hook 'sly-mrepl-mode-hook
+            (lambda ()
+              (local-set-key [remap isearch-backward] 'counsel-sly-mrepl-history)
+              (local-set-key (kbd ",,") 'counsel-sly-mrepl-shortcut)
+              (local-set-key (kbd ",p") 'sly-mrepl-set-package)
+              (local-set-key (kbd ",r") 'sly-restart-inferior-lisp)))
+  ;; Remove M-n and M-p from sly-editing-mode-map in sly.el. Don't know how else
+  ;; to enable smartscan in lisp buffers since sly overrides them.
   )
 
 ;; Open compilation results in a bottom buffer, similar to helm, identical code.
